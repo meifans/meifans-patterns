cf6f438d6ff9364cf6c51bc05e918836d1ed6474
meifans <916109363@qq.com>
2016-05-03 08:17:42 +0800

    security

--- src/main/java/github/meifans/hello/api/rest/MyRestController.java
+++ src/main/java/github/meifans/hello/api/rest/MyRestController.java
@@ -1,24 +1,30 @@
 package github.meifans.hello.api.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import github.meifans.hello.exception.MyException;
+
 @RestController
 @RequestMapping("/")
 public class MyRestController {
-
-    @RequestMapping(value = "/{meifans}", method = RequestMethod.GET)
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyRestController.class); 
+	
+    @RequestMapping(value = "/{user}", method = RequestMethod.GET)
     public String getUser(@PathVariable String user) {
         return "hello," + user;
 
     }
 
-    @RequestMapping(value = "/exception", method = RequestMethod.GET)
+    @RequestMapping(value = "/exception/{exceptionName}", method = RequestMethod.GET)
     public String getUser1(@PathVariable String exceptionName) {
-        throw new ConversionNotSupportedException(null, null, null);
+    	LOGGER.info("exceptionName is :" +exceptionName);
+        throw new MyException();
 
     }
 
