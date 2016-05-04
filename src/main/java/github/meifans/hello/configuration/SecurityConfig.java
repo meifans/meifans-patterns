97bd4a5de6612c501540d26fdcd05d45800c5492
赵鹏飞 <pengfeizhao9@creditease.cn>
2016-05-04 14:19:23 +0800

    update

--- src/main/java/github/meifans/hello/configuration/SecurityConfig.java
+++ src/main/java/github/meifans/hello/configuration/SecurityConfig.java
@@ -1,20 +1,28 @@
 package github.meifans.hello.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("user").roles("USER")
-		.and().withUser("admin").password("admin").roles("admin","AMIND");
-		
-		
-		
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER").and().withUser("admin")
+                .password("admin").roles("admin", "AMIND");
 
-	
-	
-	
+    }
+
+    // @Bean
+    // public FilterSecurityInterceptor filterSecurityInterceptor() {
+    // return new FilterSecurityInterceptor();
+    // }
+    //
+    // @Bean
+    // public UsernamePasswordAuthenticationFilter authenticationFilter() {
+    // UsernamePasswordAuthenticationFilter authenticationFilter = new
+    // UsernamePasswordAuthenticationFilter();
+    // return authenticationFilter;
+    // }
 }
