62af4c690b3a1d4afb8760c2d8dc851a7b9bb399
meifans <916109363@qq.com>
2016-10-13 23:26:21 +0800

    add ignore

--- src/main/java/github/meifans/hello/aspect/DemoInterface.java
+++
@@ -1,9 +0,0 @@
-package github.meifans.hello.aspect;
-
-/**
- * Created by Meifans on 2016/8/9.
- */
-public interface DemoInterface {
-	public void sayHello();
-
-}
--- src/test/java/github/meifan/hello/configuration/Stringbuffer.java
+++
@@ -1,8 +0,0 @@
-package github.meifan.hello.configuration;
-
-public class Stringbuffer {
-
-    public static void main(String[] args) {
-        System.out.println("hello" + "world");
-    }
-}
