b957c3c330f261dfd6e67dd85eb5044748ab1cf5
meifans <zhaopengfei@zhaopengfeideMacBook-Pro.local>
2016-12-15 17:08:49 +0800

    f

---
+++ src/main/java/github/meifans/hello/patterns/NullObject/Employee.java
@@ -0,0 +1,23 @@
+package github.meifans.hello.patterns.NullObject;
+
+import java.util.Date;
+
+/**
+ * Created by Meifans on 16/12/15.
+ */
+public interface Employee {
+
+    boolean timeToPay(Date payDate);
+
+    void pay();
+
+    Employee NULL = new Employee() {
+        public boolean timeToPay(Date payDate) {
+            return false;
+        }
+
+        public void pay() {
+        }
+    };
+
+}
---
+++ src/test/java/github/meifans/hello/patterns/NullObject/DBTest.java
@@ -0,0 +1,22 @@
+package github.meifans.hello.patterns.NullObject;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by Meifans on 16/12/15.
+ */
+public class DBTest {
+
+    @Test
+    public void testNull() {
+        Employee employee = DB.getEmployee("meifans");
+        if (employee.timeToPay(new Date())) {
+            employee.pay();
+        }
+        Assert.assertEquals(Employee.NULL, employee);
+    }
+
+}
